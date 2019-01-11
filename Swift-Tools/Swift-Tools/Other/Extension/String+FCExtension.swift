39b11dbf226cbc46b31ca86dab564f11a547c93d
Ganggang Xie <ganggangxie@q007-2.local>
2019-01-11 17:48:10 +0800

    1

--- Swift-Tools/Swift-Tools/Other/Extension/String+FCExtension.swift
+++ Swift-Tools/Swift-Tools/Other/Extension/String+FCExtension.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2019年 石富才. All rights reserved.
 //
 
+//https://www.jianshu.com/p/b23f0ac70826
+//https://www.jianshu.com/p/f03bf112840c
+//https://blog.csdn.net/nick_since2016/article/details/79218778
+
 import Foundation
 
 ///正则表达式
@@ -15,6 +19,21 @@ extension String{
     func fc_evaluateWithRegex(regex: String) -> Bool{
         let predicate: NSPredicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regex])
         return predicate.evaluate(with: self)
+    }
+    
+}
+
+///字符串截取
+extension String{
+    
+    func fc_subString(range: Range<Int>) -> String{
+        var lower = range.lowerBound
+        var upper = range.upperBound
+        if lower > upper{
+            (lower,upper) = (upper,lower)
+        }
+        
+        return ""
     }
     
 }
